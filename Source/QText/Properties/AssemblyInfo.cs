638874329719b7be53a146e8af867dc86a2fd145
Josip Medved <jmedved@jmedved.com>
2010-09-30 04:24:15 +0200

    QText Temporary version before conversion of MainForm and OptionsForm.

--- Source/QText/Properties/AssemblyInfo.cs
+++ Source/QText/Properties/AssemblyInfo.cs
@@ -5,11 +5,9 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 
-[assembly: AssemblyTitle("QText helper")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("QText")]
+[assembly: AssemblyDescription("Tool for taking quick text notes.")]
 
 [assembly: CLSCompliant(true)]
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
-
-[assembly: InternalsVisibleTo("QText, PublicKey=00240000048000009400000006020000002400005253413100040000010001002de64bdf2fb7ba60913800e843fe50288f7c1467051bb0a70eca140d1e3900530a51931ead28a80d50c7b4bd7a6ec868f601dee366fef644d129a43d6eb8090c1fd097d4e13b80b1069eafab518233e0f21e14e89ee73e47486a7faf4ea2a4ad5dd48a80d1477fdd3c1715c8b46e876bbc2c27595914a0c1437d44e656e4c2d1")]
--- Source/QText/Properties/SolutionInfo.cs
+++ Source/QText/Properties/SolutionInfo.cs
@@ -5,6 +5,6 @@ using System.Reflection;
 [assembly: AssemblyCompany("Josip Medved")]
 [assembly: AssemblyCopyright("Copyright (c) 2004-2010 Josip Medved <jmedved@jmedved.com>")]
 
-[assembly: AssemblyVersion("2.30.11568.00582")]
-[assembly: AssemblyFileVersion("2.30.11568.00582")]
+[assembly: AssemblyVersion("2.30.11568.02232")]
+[assembly: AssemblyFileVersion("2.30.11568.02232")]
 [assembly: AssemblyInformationalVersion("2.30")]
