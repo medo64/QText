f837dec5667cc7c28c89cb1a728b8976c1391ce6
Josip Medved <jmedved@jmedved.com>
2010-10-05 01:41:40 +0200

    QText: Just version update.

--- Source/QText/Properties/SolutionInfo.cs
+++ Source/QText/Properties/SolutionInfo.cs
@@ -5,6 +5,6 @@ using System.Reflection;
 [assembly: AssemblyCompany("Josip Medved")]
 [assembly: AssemblyCopyright("Copyright (c) 2004-2010 Josip Medved <jmedved@jmedved.com>")]
 
-[assembly: AssemblyVersion("2.30.11572.23374")]
-[assembly: AssemblyFileVersion("2.30.11572.23374")]
+[assembly: AssemblyVersion("2.30.11572.23410")]
+[assembly: AssemblyFileVersion("2.30.11572.23410")]
 [assembly: AssemblyInformationalVersion("2.30")]
