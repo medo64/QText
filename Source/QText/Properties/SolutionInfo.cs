3b35cb8a3aa7dfee216e1193c4d58fa0967ece20
Josip Medved <jmedved@jmedved.com>
2010-11-17 23:44:48 +0100

    QText: Removed build events.

--- Source/QText/Properties/SolutionInfo.cs
+++ Source/QText/Properties/SolutionInfo.cs
@@ -6,4 +6,4 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Copyright (c) 2004-2010 Josip Medved <jmedved@jmedved.com>")]
 
 [assembly: AssemblyVersion("2.40.*")]
-[assembly: AssemblyInformationalVersion("2.40 (alpha)")]
+[assembly: AssemblyInformationalVersion("2.40")]
