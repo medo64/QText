675e3d56795152036e62b71159cc8a1ef9e964de
Josip Medved <jmedved@jmedved.com>
2011-12-16 22:51:24 -0500

    Fixed multiline Tab and Shift+Tab.

--- Source/QText/RichTextBoxEx.cs
+++ Source/QText/RichTextBoxEx.cs
@@ -5,21 +5,11 @@ using System.Windows.Forms;
 using System.Security.Permissions;
 
 namespace QText {
-    public class RichTextBoxEx : RichTextBox {
+    internal class RichTextBoxEx : RichTextBox {
 
         public RichTextBoxEx() {
-            this.UseMultilineTab = true;
         }
 
-
-        /// <summary> 
-        /// Gets or sets whether multiline tabs will be allowed. If Multiline is not true than this property is ignored. 
-        /// </summary> 
-        [System.ComponentModel.Category("Behaviour")]
-        [System.ComponentModel.Description("Gets or sets whether multiline tabs will be allowed.")]
-        [System.ComponentModel.MergableProperty(true)]
-        [System.ComponentModel.DefaultValue(true)]
-        public bool UseMultilineTab { get; set; }
 
         protected override void OnLinkClicked(System.Windows.Forms.LinkClickedEventArgs e) {
             if (Settings.FollowURLs) {
@@ -32,55 +22,6 @@ namespace QText {
         }
 
 
-        private int _beginUpdateCount;
-        private IntPtr _originalEventMask;
-        public void BeginUpdate() {
-            this._beginUpdateCount += 1;
-            if (this._beginUpdateCount > 1) {
-                return;
-            }
-
-            _originalEventMask = NativeMethods.SendMessage(new HandleRef(this, Handle).Handle, NativeMethods.EM_SETEVENTMASK, 0, 0); // Prevent the control from raising any events.
-            NativeMethods.SendMessage(new HandleRef(this, Handle).Handle, NativeMethods.WM_SETREDRAW, 0, 0); //Prevent the control from redrawing itself.
-        }
-
-        public void EndUpdate() {
-            this._beginUpdateCount -= 1;
-            if (_beginUpdateCount > 0) { return; }
-
-            NativeMethods.SendMessage(new HandleRef(this, Handle).Handle, NativeMethods.WM_SETREDRAW, 1, 0); // Allow the control to redraw itself.
-            NativeMethods.SendMessage(new HandleRef(this, Handle).Handle, NativeMethods.EM_SETEVENTMASK, 0, _originalEventMask); // Allow the control to raise event messages.
-
-            this.Refresh();
-        }
-
-
-        private class NativeMethods {
-
-            internal const int WM_SETREDRAW = 11;
-            internal const int EM_SETEVENTMASK = 1073;
-
-            public static IntPtr SendMessage(IntPtr hWnd, uint Msg, int wParam, IntPtr lParam) {
-                return SendMessageW(hWnd, Msg, new IntPtr(wParam), ref lParam);
-            }
-
-            public static IntPtr SendMessage(IntPtr hWnd, uint Msg, int wParam, int lParam) {
-                var x = new IntPtr(lParam);
-                return SendMessageW(hWnd, Msg, new IntPtr(wParam), ref x);
-            }
-
-            [System.Runtime.InteropServices.DllImportAttribute("user32.dll", EntryPoint = "SendMessageW")]
-            public static extern IntPtr SendMessageW([System.Runtime.InteropServices.InAttribute()] System.IntPtr hWnd, uint Msg, IntPtr wParam, ref IntPtr lParam);
-
-        }
-
-
-        /// <summary> 
-        /// Processes a command key. 
-        /// </summary> 
-        /// <param name="msg">A System.Windows.Forms.Message, passed by reference, that represents the window message to process.</param> 
-        /// <param name="keyData">One of the System.Windows.Forms.Keys values that represents the key to process.</param> 
-        /// <returns>True if the character was processed by the control; otherwise, false.</returns> 
         [SecurityPermission(SecurityAction.LinkDemand, Flags = SecurityPermissionFlag.UnmanagedCode)]
         protected override bool ProcessCmdKey(ref System.Windows.Forms.Message msg, System.Windows.Forms.Keys keyData) {
             switch (keyData) {
@@ -127,94 +68,122 @@ namespace QText {
                     } return true;
 
                 case System.Windows.Forms.Keys.Tab: {
-
-                        if ((this.Multiline) && (this.UseMultilineTab)) {
-                            int selStart = base.SelectionStart;
-                            int selLength = base.SelectionLength;
-                            int lineFrom = base.GetLineFromCharIndex(selStart);
-                            int lineTo = base.GetLineFromCharIndex(selStart + selLength);
-                            if (lineFrom == lineTo) {
-                                //tab within single line 
-                                base.SelectedText = System.Convert.ToChar(9).ToString();
-                            } else {
-                                //multiline tab 
-                                string[] lines = base.Lines;
-                                int lineToM = System.Math.Min(base.GetLineFromCharIndex(selStart + selLength - 1), lines.Length - 1);
-                                for (int i = lineFrom; i <= lineToM; i++) {
-                                    lines[i] = System.Convert.ToChar(9) + lines[i];
-                                }
-                                base.Lines = lines;
-                                base.SelectionStart = selStart + 1;
-                                if (lineTo == lineToM) {
-                                    base.SelectionLength = selLength + (lineTo - lineFrom);
-                                } else {
-                                    base.SelectionLength = selLength + (lineToM - lineFrom);
-                                }
+                        var startLine = this.GetLineFromCharIndex(this.SelectionStart);
+                        var endLine = this.GetLineFromCharIndex(this.SelectionStart + this.SelectionLength - 1);
+                        if (startLine == endLine) { //single line tab
+                            this.SelectedText = "\t";
+                        } else {
+                            var origStart = this.SelectionStart;
+                            var origLen = this.SelectionLength;
+                            var startIndex = this.GetFirstCharIndexFromLine(startLine);
+                            var endIndex = this.GetFirstCharIndexFromLine(endLine + 1) - 1;
+                            if ((this.Text[endIndex] == '\r') || (this.Text[endIndex] == '\n')) { endIndex -= 1; } //if this is CRFL
+                            var text = this.Text.Substring(startIndex, endIndex - startIndex);
+                            var endOfLine = "\n"; if (text.Contains("\r\n")) { endOfLine = "\r\n"; } //to see whether LF or CRLF is used.
+                            var lines = text.Split(new string[] { endOfLine }, StringSplitOptions.None);
+                            for (int i = 0; i < lines.Length; i++) {
+                                lines[i] = "\t" + lines[i];
                             }
-                            return true;
+                            this.BeginUpdate();
+                            this.SelectionStart = startIndex;
+                            this.SelectionLength = endIndex - startIndex;
+                            this.SelectedText = string.Join(endOfLine, lines);
+                            this.SelectionStart = (origStart == startIndex) ? origStart : origStart + 1;
+                            this.SelectionLength = (origStart == startIndex) ? origLen + lines.Length * (endOfLine.Length) : origLen + lines.Length * (endOfLine.Length) - 1;
+                            this.EndUpdate();
                         }
-                        break; // TODO: might not be correct. Was : Exit Select
-                    }
+                    } return true;
 
                 case System.Windows.Forms.Keys.Shift | System.Windows.Forms.Keys.Tab: {
-
-                        if ((this.Multiline) && (this.UseMultilineTab)) {
-                            int selStart = base.SelectionStart;
-                            int selLength = base.SelectionLength;
-                            int lineFrom = base.GetLineFromCharIndex(selStart);
-                            int lineTo = base.GetLineFromCharIndex(selStart + selLength);
-                            if (lineFrom == lineTo) {
-                                //single line tab back 
-                                int selFirst = base.GetFirstCharIndexFromLine(lineTo);
-                                if (selStart > selFirst) {
-                                    string[] lines = base.Lines;
-                                    if (lines[lineFrom][selStart - selFirst - 1] == System.Convert.ToChar(9)) {
-                                        lines[lineFrom] = lines[lineFrom].Remove(selStart - selFirst - 1, 1);
-                                        base.Lines = lines;
-                                        base.SelectionStart = selStart - 1;
-                                    }
-                                }
-                            } else {
-                                //multiline tab back 
-                                string[] lines = base.Lines;
-                                int lineToM = System.Math.Min(base.GetLineFromCharIndex(selStart + selLength - 1), lines.Length - 1);
-                                int tabCount = 0;
-                                bool tabInFirstLine = false;
-                                for (int i = lineFrom; i <= lineToM; i++) {
-                                    if (lines[i].StartsWith(System.Convert.ToChar(9).ToString())) {
-                                        if (i == lineFrom) {
-                                            tabInFirstLine = true;
-                                        }
-                                        lines[i] = lines[i].Remove(0, 1);
-                                        tabCount += 1;
-                                    }
-                                }
-                                if (tabCount > 0) {
-                                    base.Lines = lines;
-                                    if (selStart == 0) {
-                                        base.SelectionStart = 0;
-                                    } else {
-                                        if (tabInFirstLine == true) {
-                                            base.SelectionStart = selStart - 1;
-                                        } else {
-                                            base.SelectionStart = selStart;
-                                        }
-                                    }
-                                    if (tabCount <= (lineToM - lineFrom)) {
-                                        base.SelectionLength = selLength - tabCount;
-                                    } else {
-                                        base.SelectionLength = selLength - (lineToM - lineFrom);
-                                    }
+                        var startLine = this.GetLineFromCharIndex(this.SelectionStart);
+                        var endLine = this.GetLineFromCharIndex(this.SelectionStart + this.SelectionLength - 1);
+                        var origStart = this.SelectionStart;
+                        var origLen = this.SelectionLength;
+                        if (startLine == endLine) { //single line tab
+                            if (this.SelectionStart > 0) {
+                                if (this.Text[this.SelectionStart - 1] == '\t') {
+                                    this.BeginUpdate();
+                                    this.SelectionStart -= 1;
+                                    this.SelectionLength = 1;
+                                    this.SelectedText = "";
+                                    this.SelectionStart = origStart - 1;
+                                    this.SelectionLength = origLen;
+                                    this.EndUpdate();
                                 }
                             }
-                            return true;
+                        } else {
+                            var startIndex = this.GetFirstCharIndexFromLine(startLine);
+                            var endIndex = this.GetFirstCharIndexFromLine(endLine + 1) - 1;
+                            if ((this.Text[endIndex] == '\r') || (this.Text[endIndex] == '\n')) { endIndex -= 1; } //if this is CRFL
+                            var text = this.Text.Substring(startIndex, endIndex - startIndex);
+                            var endOfLine = "\n"; if (text.Contains("\r\n")) { endOfLine = "\r\n"; } //to see whether LF or CRLF is used.
+                            var lines = text.Split(new string[] { endOfLine }, StringSplitOptions.None);
+                            var countRemoved = 0;
+                            for (int i = 0; i < lines.Length; i++) {
+                                if (lines[i].StartsWith("\t", StringComparison.InvariantCultureIgnoreCase)) {
+                                    lines[i] = lines[i].Substring(1);
+                                    countRemoved += 1;
+                                }
+                            }
+                            if (countRemoved > 0) {
+                                this.BeginUpdate();
+                                this.SelectionStart = startIndex;
+                                this.SelectionLength = endIndex - startIndex;
+                                this.SelectedText = string.Join(endOfLine, lines);
+                                this.SelectionStart = (origStart == startIndex) ? origStart : origStart - 1;
+                                this.SelectionLength = (origStart == startIndex) ? origLen - countRemoved : origLen - countRemoved + 1;
+                                this.EndUpdate();
+                            }
                         }
-                        break; // TODO: might not be correct. Was : Exit Select
-                    }
+                    } return true;
+
 
             }
 
             return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+
+        private int _beginUpdateCount;
+        private IntPtr _originalEventMask;
+        public void BeginUpdate() {
+            this._beginUpdateCount += 1;
+            if (this._beginUpdateCount > 1) {
+                return;
+            }
+
+            _originalEventMask = NativeMethods.SendMessage(new HandleRef(this, Handle).Handle, NativeMethods.EM_SETEVENTMASK, 0, 0); // Prevent the control from raising any events.
+            NativeMethods.SendMessage(new HandleRef(this, Handle).Handle, NativeMethods.WM_SETREDRAW, 0, 0); //Prevent the control from redrawing itself.
+        }
+
+        public void EndUpdate() {
+            this._beginUpdateCount -= 1;
+            if (_beginUpdateCount > 0) { return; }
+
+            NativeMethods.SendMessage(new HandleRef(this, Handle).Handle, NativeMethods.WM_SETREDRAW, 1, 0); // Allow the control to redraw itself.
+            NativeMethods.SendMessage(new HandleRef(this, Handle).Handle, NativeMethods.EM_SETEVENTMASK, 0, _originalEventMask); // Allow the control to raise event messages.
+
+            this.Refresh();
+        }
+
+
+        private class NativeMethods {
+
+            internal const int WM_SETREDRAW = 11;
+            internal const int EM_SETEVENTMASK = 1073;
+
+            public static IntPtr SendMessage(IntPtr hWnd, uint Msg, int wParam, IntPtr lParam) {
+                return SendMessageW(hWnd, Msg, new IntPtr(wParam), ref lParam);
+            }
+
+            public static IntPtr SendMessage(IntPtr hWnd, uint Msg, int wParam, int lParam) {
+                var x = new IntPtr(lParam);
+                return SendMessageW(hWnd, Msg, new IntPtr(wParam), ref x);
+            }
+
+            [System.Runtime.InteropServices.DllImportAttribute("user32.dll", EntryPoint = "SendMessageW")]
+            public static extern IntPtr SendMessageW([System.Runtime.InteropServices.InAttribute()] System.IntPtr hWnd, uint Msg, IntPtr wParam, ref IntPtr lParam);
+
         }
 
     }
