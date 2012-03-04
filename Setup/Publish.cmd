e33a4e330670572aaa85bb89421de40b4e59af93
Josip Medved <jmedved@jmedved.com>
2012-03-04 01:57:41 -0500

    Fixing-up script.

--- Setup/Publish.cmd
+++ Setup/Publish.cmd
@@ -8,8 +8,9 @@ ECHO --- Sign executables
 ECHO.
 ECHO --- Setup
 
+RMDIR /Q /S ".\Temp" 2> NUL
 CALL "%PROGRAMFILES(x86)%\Inno Setup 5\iscc.exe" /O".\Temp" ".\QText.iss"
-IF NOT ERRORLEVEL 0 EXIT /B %ERRORLEVEL%
+IF ERRORLEVEL 1 EXIT /B %ERRORLEVEL%
 
 FOR /F %%I IN ('DIR ".\Temp\*.exe" /B') DO SET SETUPEXE=%%I
 ECHO Setup is in file %SETUPEXE%
@@ -24,16 +25,16 @@ ECHO --- Zip
 
 SET SETUPZIP=%SETUPEXE:.exe=.zip%
 ECHO Zipping into %SETUPZIP%
-"%PROGRAMFILES%\WinRAR\WinRAR.exe" a -afzip -ep ".\Temp\%SETUPZIP%" "..\Binaries\QText.exe" "..\Binaries\Readme.txt"
-IF NOT ERRORLEVEL 0 EXIT /B %ERRORLEVEL%
+"%PROGRAMFILES%\WinRAR\WinRAR.exe" a -afzip -ep -m5 ".\Temp\%SETUPZIP%" "..\Binaries\QText.exe" "..\Binaries\Readme.txt"
+IF ERRORLEVEL 1 EXIT /B %ERRORLEVEL%
 
 
 ECHO.
 ECHO --- Move to releases
 
 MOVE ".\Temp\*.*" "..\Releases\."
-IF NOT ERRORLEVEL 0 EXIT /B %ERRORLEVEL%
-RMDIR ".\Temp"
+IF ERRORLEVEL 1 EXIT /B %ERRORLEVEL%
+RMDIR /Q /S ".\Temp"
 
 
 ECHO.
