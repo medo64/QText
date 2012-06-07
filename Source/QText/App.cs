4d28d3b6c8923f2b29a3a17403d82ed2faa88172
Josip Medved <jmedved@jmedved.com>
2012-06-06 20:53:36 -0700

    Fixing setup mutex.

--- Source/QText/App.cs
+++ Source/QText/App.cs
@@ -3,6 +3,8 @@ using System.ComponentModel;
 using System.Diagnostics;
 using System.Globalization;
 using System.Runtime.InteropServices;
+using System.Security.AccessControl;
+using System.Security.Principal;
 using System.Threading;
 using System.Windows.Forms;
 
@@ -10,7 +12,6 @@ namespace QText {
 
     internal static class App {
 
-        public static Mutex SetupMutex;
         public static MainForm Form;
         public static Tray Tray;
 
@@ -19,84 +20,83 @@ namespace QText {
 
         [STAThread]
         public static void Main() {
-            try {
-                App.SetupMutex = new Mutex(false, @"Global\JosipMedved_QText");
-            } catch (UnauthorizedAccessException) { }
+            bool createdNew;
+            var mutexSecurity = new MutexSecurity();
+            mutexSecurity.AddAccessRule(new MutexAccessRule(new SecurityIdentifier(WellKnownSidType.WorldSid, null), MutexRights.FullControl, AccessControlType.Allow));
+            using (var setupMutex = new Mutex(false, @"Global\JosipMedved_QText", out createdNew, mutexSecurity)) {
 
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Thread.CurrentThread.CurrentUICulture = new CultureInfo(Medo.Configuration.Settings.Read("CultureName", "en-US"));
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo(Medo.Configuration.Settings.Read("CultureName", "en-US"));
 
-            if (Medo.Application.Args.Current.ContainsKey("setup")) {
-                Legacy.Settings.Upgrade();
-                Legacy.OrderedFiles.Upgrade();
-                Legacy.HiddenFiles.Upgrade();
-            }
-
-            Medo.Application.UnhandledCatch.ThreadException += new System.EventHandler<ThreadExceptionEventArgs>(UnhandledException);
-            Medo.Application.UnhandledCatch.Attach();
-
-            Application.ApplicationExit += new System.EventHandler(ApplicationExit);
-
-            Medo.Configuration.Settings.NoRegistryWrites = !Settings.Installed;
-            Medo.Windows.Forms.State.NoRegistryWrites = !Settings.Installed;
-
-            App.Form = new MainForm();
-
-            Medo.Application.SingleInstance.NewInstanceDetected += new EventHandler<Medo.Application.NewInstanceEventArgs>(SingleInstance_NewInstanceDetected);
-            if (Medo.Application.SingleInstance.IsOtherInstanceRunning) {
-                var currProcess = Process.GetCurrentProcess();
-                foreach (var iProcess in Process.GetProcessesByName(currProcess.ProcessName)) {
-                    try {
-                        if (iProcess.Id != currProcess.Id) {
-                            NativeMethods.AllowSetForegroundWindow(iProcess.Id);
-                            break;
-                        }
-                    } catch (Win32Exception) { }
+                if (Medo.Application.Args.Current.ContainsKey("setup")) {
+                    Legacy.Settings.Upgrade();
+                    Legacy.OrderedFiles.Upgrade();
+                    Legacy.HiddenFiles.Upgrade();
                 }
-            }
-            Medo.Application.SingleInstance.Attach();
 
+                Medo.Application.UnhandledCatch.ThreadException += new System.EventHandler<ThreadExceptionEventArgs>(UnhandledException);
+                Medo.Application.UnhandledCatch.Attach();
 
-            try {
-                Helper.CreatePath(Settings.FilesLocation);
-            } catch (Exception ex) {
-                switch (Medo.MessageBox.ShowQuestion(null, ex.Message + Environment.NewLine + "Do you wish to try using default location instead?", MessageBoxButtons.YesNo)) {
-                    case DialogResult.Yes:
+                Application.ApplicationExit += new System.EventHandler(ApplicationExit);
+
+                Medo.Configuration.Settings.NoRegistryWrites = !Settings.Installed;
+                Medo.Windows.Forms.State.NoRegistryWrites = !Settings.Installed;
+
+                App.Form = new MainForm();
+
+                Medo.Application.SingleInstance.NewInstanceDetected += new EventHandler<Medo.Application.NewInstanceEventArgs>(SingleInstance_NewInstanceDetected);
+                if (Medo.Application.SingleInstance.IsOtherInstanceRunning) {
+                    var currProcess = Process.GetCurrentProcess();
+                    foreach (var iProcess in Process.GetProcessesByName(currProcess.ProcessName)) {
                         try {
-                            string defaultPath = Settings.DefaultFilesLocation;
-                            Helper.CreatePath(defaultPath);
-                            Settings.FilesLocation = defaultPath;
-                        } catch (Exception ex2) {
-                            global::Medo.MessageBox.ShowError(null, ex2.Message, MessageBoxButtons.OK);
-                            Application.Exit();
-                            System.Environment.Exit(0);
-                        }
-                        break;
-                    case DialogResult.No:
-                        break;
+                            if (iProcess.Id != currProcess.Id) {
+                                NativeMethods.AllowSetForegroundWindow(iProcess.Id);
+                                break;
+                            }
+                        } catch (Win32Exception) { }
+                    }
                 }
-            }
+                Medo.Application.SingleInstance.Attach();
 
-            App.Tray = new Tray(App.Form);
-            App.Tray.Show();
 
-            App.Hotkey.HotkeyActivated += new EventHandler<EventArgs>(Hotkey_HotkeyActivated);
-            if (Settings.ActivationHotkey != Keys.None) {
                 try {
-                    App.Hotkey.Register(Settings.ActivationHotkey);
-                } catch (InvalidOperationException) {
-                    Medo.MessageBox.ShowWarning(null, "Hotkey is already in use.");
+                    Helper.CreatePath(Settings.FilesLocation);
+                } catch (Exception ex) {
+                    switch (Medo.MessageBox.ShowQuestion(null, ex.Message + Environment.NewLine + "Do you wish to try using default location instead?", MessageBoxButtons.YesNo)) {
+                        case DialogResult.Yes:
+                            try {
+                                string defaultPath = Settings.DefaultFilesLocation;
+                                Helper.CreatePath(defaultPath);
+                                Settings.FilesLocation = defaultPath;
+                            } catch (Exception ex2) {
+                                global::Medo.MessageBox.ShowError(null, ex2.Message, MessageBoxButtons.OK);
+                                Application.Exit();
+                                System.Environment.Exit(0);
+                            }
+                            break;
+                        case DialogResult.No:
+                            break;
+                    }
                 }
+
+                App.Tray = new Tray(App.Form);
+                App.Tray.Show();
+
+                App.Hotkey.HotkeyActivated += new EventHandler<EventArgs>(Hotkey_HotkeyActivated);
+                if (Settings.ActivationHotkey != Keys.None) {
+                    try {
+                        App.Hotkey.Register(Settings.ActivationHotkey);
+                    } catch (InvalidOperationException) {
+                        Medo.MessageBox.ShowWarning(null, "Hotkey is already in use.");
+                    }
+                }
+
+                if (Medo.Application.Args.Current.ContainsKey("hide") == false) {
+                    Tray.ShowForm();
+                }
+                Application.Run();
             }
-
-            if (Medo.Application.Args.Current.ContainsKey("hide") == false) {
-                Tray.ShowForm();
-            }
-            Application.Run();
-
-
-            System.GC.KeepAlive(App.SetupMutex);
         }
 
 
