0b579076306d2df3a6314d371bea14e02608d330
Josip Medved <jmedved@jmedved.com>
2012-09-16 21:59:50 -0700

    Added retry upon send failure and check for upgrade after unhandled exception.

--- Source/QText/App.cs
+++ Source/QText/App.cs
@@ -135,6 +135,7 @@ namespace QText {
             if (Settings.Installed) { Medo.Diagnostics.ErrorReport.SaveToTemp(e.Exception); }
 #if !DEBUG
             Medo.Diagnostics.ErrorReport.ShowDialog(null, e.Exception, new Uri("http://jmedved.com/feedback/"));
+            Medo.Services.Upgrade.ShowDialog(null, new Uri("http://jmedved.com/upgrade/"));
 #else
             throw e.Exception;
 #endif
