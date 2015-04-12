09e4844a9417f2a128e700162ae8bfacf1ebee00
Josip Medved <jmedved@jmedved.com>
2015-04-12 11:59:11 -0700

    Added basic document test.

---
+++ Source/QText.Test/(Helper)/TestDirectory.cs
@@ -0,0 +1,33 @@
+using System;
+using System.IO;
+
+namespace QTextTest {
+    internal class TestDirectory : IDisposable {
+
+        public TestDirectory() {
+            this.Directory = new DirectoryInfo(Path.Combine(Path.GetTempPath(), "QText.Test " + Guid.NewGuid().ToString()));
+            this.Directory.Create();
+        }
+
+        ~TestDirectory() {
+            ((IDisposable)this).Dispose();
+        }
+
+
+        public DirectoryInfo Directory { get; private set; }
+
+
+        public void CreateRawFile(string fileName) {
+            var file = new FileInfo(Path.Combine(this.Directory.FullName, fileName));
+            if (!file.Directory.Exists) { file.Directory.Create(); }
+            file.Create().Close();
+        }
+
+
+        void IDisposable.Dispose() {
+            if (this.Directory.Exists) { this.Directory.Delete(true); }
+            GC.SuppressFinalize(this);
+        }
+
+    }
+}
---
+++ Source/QText.Test/DocumentTests.cs
@@ -0,0 +1,49 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QText;
+using System.Collections.Generic;
+
+namespace QTextTest {
+
+    [TestClass()]
+    public class DocumentTests {
+
+        public TestContext TestContext { get; set; }
+
+
+        [TestMethod()]
+        public void Document_OpenPreExisting() {
+            using (var test = new TestDirectory()) {
+                test.CreateRawFile("A.txt");
+                test.CreateRawFile("B.txt");
+                test.CreateRawFile("C.txt");
+                test.CreateRawFile("Inner\\C.txt");
+
+                var doc = new Document(test.Directory);
+                var folders = new List<DocumentFolder>(doc.GetFolders());
+
+                Assert.AreEqual(2, folders.Count);
+                Assert.AreEqual("", folders[0].Name);
+                Assert.AreEqual("Inner", folders[1].Name);
+
+                {
+                    var files = new List<DocumentFile>(folders[0].GetFiles());
+                    Assert.AreEqual(3, files.Count);
+                    Assert.AreEqual("A", files[0].Title);
+                    Assert.AreEqual("B", files[1].Title);
+                    Assert.AreEqual("C", files[2].Title);
+                }
+
+                {
+                    var files = new List<DocumentFile>(folders[1].GetFiles());
+                    Assert.AreEqual(1, files.Count);
+                    Assert.AreEqual("C", files[0].Title);
+                }
+            }
+        }
+
+
+
+
+
+    }
+}
