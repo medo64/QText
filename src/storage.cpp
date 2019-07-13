1edc68454e8f58c6ff7da2149a9daf742487c01c
Josip Medved <jmedved@jmedved.com>
2019-07-13 13:08:16 -0700

    Supporting home directory (~) as storage path element

--- src/storage.cpp
+++ src/storage.cpp
@@ -3,17 +3,17 @@
 #include <QDir>
 
 Storage::Storage(const QString path) {
-    QDir rootDirectory = path;
+    QDir rootDirectory = path.startsWith("~/") ? QDir::homePath() + path.mid(1) : path;
     if (!rootDirectory.exists()) {
         rootDirectory.mkpath(path);
     }
 
     _path = rootDirectory.path();
 
-    _folders.push_back(std::make_shared<FolderItem>(path, nullptr));
+    _folders.push_back(std::make_shared<FolderItem>(_path, nullptr));
     QStringList directories = rootDirectory.entryList(QDir::Dirs|QDir::NoDotAndDotDot, QDir::SortFlag::Name);
     for(QString directory : directories) {
-        auto folder = std::make_shared<FolderItem>(path, directory);
+        auto folder = std::make_shared<FolderItem>(_path, directory);
         _folders.push_back(folder);
     }
 }
