2c1b883cf34b9b74666a1decdb3e127778a63575
Josip Medved <jmedved@jmedved.com>
2019-11-17 09:29:51 -0800

    Upgrade and ErrorReport to use TLS 1.3

--- Source/QText/MainForm.cs
+++ Source/QText/MainForm.cs
@@ -1711,7 +1711,7 @@ namespace QText {
         }
 
         private void bwCheckForUpgrade_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e) {
-            if (!e.Cancelled) {
+            if (!e.Cancelled && (e.Error == null)) {
                 Helper.ScaleToolstripItem(mnuApp, "mnuAppUpgrade");
                 mnuAppUpgrade.Text = "Upgrade is available";
 
