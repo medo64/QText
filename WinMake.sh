01ef8ffe2dc1fbe194b16f171a96d27f2daff780
Josip Medved <jmedved@jmedved.com>
2020-05-23 15:37:12 -0700

    Added clean to WinMake.sh

--- WinMake.sh
+++ WinMake.sh
@@ -19,6 +19,7 @@ fi
 
 case $1 in
     '')        BUILD='release' ; DO_PACKAGE=0 ;;
+    'clean')   BUILD=''        ; DO_PACKAGE=0 ;;
     'debug')   BUILD='debug'   ; DO_PACKAGE=0 ;;
     'release') BUILD='release' ; DO_PACKAGE=0 ;;
     'test')    BUILD='test'    ; DO_PACKAGE=0 ;;
@@ -82,124 +83,123 @@ fi
 HAS_UNCOMMITTED_RESULT=`git diff --quiet ; echo $?`
 
 
+rm bin/* 2> /dev/null
+mkdir -p bin
 rm -R build/ 2> /dev/null
 mkdir -p build
 cd build
 
-PATH=$PATH:`dirname $CMD_MAKE`
 
-$CMD_QMAKE -spec win32-g++ CONFIG+=$BUILD ../src/QText.pro
-if [[ $? -ne 0 ]]; then
-    echo -e "${ESCAPE_ERROR}QMake failed!${ESCAPE_RESET}" >&2
-    exit 1
-fi
+if [[ "$BUILD" != "" ]]; then
+    PATH=$PATH:`dirname $CMD_MAKE`
 
-$CMD_MAKE -f Makefile
-if [[ $? -ne 0 ]]; then
-    echo -e "${ESCAPE_ERROR}Make failed!${ESCAPE_RESET}" >&2
-    exit 1
-fi
+    $CMD_QMAKE -spec win32-g++ CONFIG+=$BUILD ../src/QText.pro
+    if [[ $? -ne 0 ]]; then
+        echo -e "${ESCAPE_ERROR}QMake failed!${ESCAPE_RESET}" >&2
+        exit 1
+    fi
+
+    $CMD_MAKE -f Makefile
+    if [[ $? -ne 0 ]]; then
+        echo -e "${ESCAPE_ERROR}Make failed!${ESCAPE_RESET}" >&2
+        exit 1
+    fi
 
 
-case $BUILD in
-    'release')
-        rm ../bin/* 2> /dev/null
-        mkdir -p ../bin
+    case $BUILD in
+        'release')
+            cp release/qtext.exe              ../bin/QText.exe
+            cp $QMAKE_DIR/libgcc_s_seh-1.dll  ../bin/
+            cp $QMAKE_DIR/libstdc++-6.dll     ../bin/
+            cp $QMAKE_DIR/libwinpthread-1.dll ../bin/
+            cp $QMAKE_DIR/Qt5Core.dll         ../bin/
+            cp $QMAKE_DIR/Qt5Gui.dll          ../bin/
+            cp $QMAKE_DIR/Qt5Network.dll      ../bin/
+            cp $QMAKE_DIR/Qt5PrintSupport.dll ../bin/
+            cp $QMAKE_DIR/Qt5Widgets.dll      ../bin/
 
-        cp release/qtext.exe              ../bin/QText.exe
-        cp $QMAKE_DIR/libgcc_s_seh-1.dll  ../bin/
-        cp $QMAKE_DIR/libstdc++-6.dll     ../bin/
-        cp $QMAKE_DIR/libwinpthread-1.dll ../bin/
-        cp $QMAKE_DIR/Qt5Core.dll         ../bin/
-        cp $QMAKE_DIR/Qt5Gui.dll          ../bin/
-        cp $QMAKE_DIR/Qt5Network.dll      ../bin/
-        cp $QMAKE_DIR/Qt5PrintSupport.dll ../bin/
-        cp $QMAKE_DIR/Qt5Widgets.dll      ../bin/
-
-        if [[ "$CERTIFICATE_THUMBPRINT" != "" ]]; then
-            echo
-            if [[ "$TIMESTAMP_URL" != "" ]]; then
-                "$CMD_SIGNTOOL" sign -s "My" -sha1 $CERTIFICATE_THUMBPRINT -tr $TIMESTAMP_URL -v ../bin/QText.exe
-            else
-                "$CMD_SIGNTOOL" sign -s "My" -sha1 $CERTIFICATE_THUMBPRINT -v ../bin/QText.exe
-            fi
-        fi
-
-        if [[ $HAS_UNCOMMITTED_RESULT -ne 0 ]] && [[ "$BUILD" == 'release' ]]; then
-            echo -e "${ESCAPE_WARNING}Uncommitted changes present.${ESCAPE_RESET}" >&2
-        fi
-
-        echo -e "${ESCAPE_RESULT}Release build completed.${ESCAPE_RESET}" >&2
-
-        if [[ $DO_PACKAGE -ne 0 ]]; then
-            echo
-
-            INNOSETUP_PATH='/c/Program Files (x86)/Inno Setup 6/ISCC.exe'
-
-            if [[ ! -f "$INNOSETUP_PATH" ]]; then
-                echo -e "${ESCAPE_ERROR}Cannot find InnoSetup 6!${ESCAPE_RESET}" >&2
-                exit 1
+            if [[ "$CERTIFICATE_THUMBPRINT" != "" ]]; then
+                echo
+                if [[ "$TIMESTAMP_URL" != "" ]]; then
+                    "$CMD_SIGNTOOL" sign -s "My" -sha1 $CERTIFICATE_THUMBPRINT -tr $TIMESTAMP_URL -v ../bin/QText.exe
+                else
+                    "$CMD_SIGNTOOL" sign -s "My" -sha1 $CERTIFICATE_THUMBPRINT -v ../bin/QText.exe
+                fi
             fi
 
-            cd ..
-            "$INNOSETUP_PATH" package/win/QText.iss
-            if [[ $? -eq 0 ]]; then
-                LAST_PACKAGE=`ls -t dist/*.exe | head -1`
+            if [[ $HAS_UNCOMMITTED_RESULT -ne 0 ]] && [[ "$BUILD" == 'release' ]]; then
+                echo -e "${ESCAPE_WARNING}Uncommitted changes present.${ESCAPE_RESET}" >&2
+            fi
 
-                if [[ "$CERTIFICATE_THUMBPRINT" != "" ]]; then
-                    echo
-                    if [[ "$TIMESTAMP_URL" != "" ]]; then
-                        "$CMD_SIGNTOOL" sign -s "My" -sha1 $CERTIFICATE_THUMBPRINT -tr $TIMESTAMP_URL -v $LAST_PACKAGE
-                    else
-                        "$CMD_SIGNTOOL" sign -s "My" -sha1 $CERTIFICATE_THUMBPRINT -v $LAST_PACKAGE
-                    fi
+            echo -e "${ESCAPE_RESULT}Release build completed.${ESCAPE_RESET}" >&2
+
+            if [[ $DO_PACKAGE -ne 0 ]]; then
+                echo
+
+                INNOSETUP_PATH='/c/Program Files (x86)/Inno Setup 6/ISCC.exe'
+
+                if [[ ! -f "$INNOSETUP_PATH" ]]; then
+                    echo -e "${ESCAPE_ERROR}Cannot find InnoSetup 6!${ESCAPE_RESET}" >&2
+                    exit 1
                 fi
 
-                echo
-                echo -e "${ESCAPE_RESULT}Package created ($LAST_PACKAGE).${ESCAPE_RESET}" >&2
-            else
-                echo -e "${ESCAPE_ERROR}Packaging failed!${ESCAPE_RESET}" >&2
+                cd ..
+                "$INNOSETUP_PATH" package/win/QText.iss
+                if [[ $? -eq 0 ]]; then
+                    LAST_PACKAGE=`ls -t dist/*.exe | head -1`
+
+                    if [[ "$CERTIFICATE_THUMBPRINT" != "" ]]; then
+                        echo
+                        if [[ "$TIMESTAMP_URL" != "" ]]; then
+                            "$CMD_SIGNTOOL" sign -s "My" -sha1 $CERTIFICATE_THUMBPRINT -tr $TIMESTAMP_URL -v $LAST_PACKAGE
+                        else
+                            "$CMD_SIGNTOOL" sign -s "My" -sha1 $CERTIFICATE_THUMBPRINT -v $LAST_PACKAGE
+                        fi
+                    fi
+
+                    echo
+                    echo -e "${ESCAPE_RESULT}Package created ($LAST_PACKAGE).${ESCAPE_RESET}" >&2
+                else
+                    echo -e "${ESCAPE_ERROR}Packaging failed!${ESCAPE_RESET}" >&2
+                    exit 1
+                fi
+            fi
+            ;;
+
+        'debug')
+            cp debug/qtext.exe                 ../bin/QText.exe
+            cp $QMAKE_DIR/libgcc_s_seh-1.dll   ../bin/
+            cp $QMAKE_DIR/libstdc++-6.dll      ../bin/
+            cp $QMAKE_DIR/libwinpthread-1.dll  ../bin/
+            cp $QMAKE_DIR/Qt5Cored.dll         ../bin/
+            cp $QMAKE_DIR/Qt5Guid.dll          ../bin/
+            cp $QMAKE_DIR/Qt5Networkd.dll      ../bin/
+            cp $QMAKE_DIR/Qt5PrintSupportd.dll ../bin/
+            cp $QMAKE_DIR/Qt5Widgetsd.dll      ../bin/
+
+            echo -e "${ESCAPE_RESULT}Debug build completed.${ESCAPE_RESET}" >&2
+            ;;
+
+        'test')
+            cp $QMAKE_DIR/libgcc_s_seh-1.dll  release/
+            cp $QMAKE_DIR/libstdc++-6.dll     release/
+            cp $QMAKE_DIR/libwinpthread-1.dll release/
+            cp $QMAKE_DIR/Qt5Core.dll         release/
+            cp $QMAKE_DIR/Qt5Gui.dll          release/
+            cp $QMAKE_DIR/Qt5Network.dll      release/
+            cp $QMAKE_DIR/Qt5PrintSupport.dll release/
+            cp $QMAKE_DIR/Qt5Test.dll         release/
+            cp $QMAKE_DIR/Qt5Widgets.dll      release/
+
+            echo
+            cd release
+            ./qtexttests.exe \
+                | GREP_COLOR='01;31' grep --color=always -e '^FAIL! ' -e '^' \
+                | GREP_COLOR='01;32' grep --color=always -e '^PASS ' -e '^'
+            if [[ $? -ne 0 ]]; then
+                echo -e "${ESCAPE_ERROR}Testing failed!${ESCAPE_RESET}" >&2
                 exit 1
             fi
-        fi
-        ;;
-
-    'debug')
-        rm ../bin/* 2> /dev/null
-        mkdir -p ../bin
-
-        cp debug/qtext.exe                 ../bin/QText.exe
-        cp $QMAKE_DIR/libgcc_s_seh-1.dll   ../bin/
-        cp $QMAKE_DIR/libstdc++-6.dll      ../bin/
-        cp $QMAKE_DIR/libwinpthread-1.dll  ../bin/
-        cp $QMAKE_DIR/Qt5Cored.dll         ../bin/
-        cp $QMAKE_DIR/Qt5Guid.dll          ../bin/
-        cp $QMAKE_DIR/Qt5Networkd.dll      ../bin/
-        cp $QMAKE_DIR/Qt5PrintSupportd.dll ../bin/
-        cp $QMAKE_DIR/Qt5Widgetsd.dll      ../bin/
-
-        echo -e "${ESCAPE_RESULT}Debug build completed.${ESCAPE_RESET}" >&2
-        ;;
-
-    'test')
-        cp $QMAKE_DIR/libgcc_s_seh-1.dll  release/
-        cp $QMAKE_DIR/libstdc++-6.dll     release/
-        cp $QMAKE_DIR/libwinpthread-1.dll release/
-        cp $QMAKE_DIR/Qt5Core.dll         release/
-        cp $QMAKE_DIR/Qt5Gui.dll          release/
-        cp $QMAKE_DIR/Qt5Network.dll      release/
-        cp $QMAKE_DIR/Qt5PrintSupport.dll release/
-        cp $QMAKE_DIR/Qt5Test.dll         release/
-        cp $QMAKE_DIR/Qt5Widgets.dll      release/
-
-        echo
-        cd release
-        ./qtexttests.exe \
-            | GREP_COLOR='01;31' grep --color=always -e '^FAIL! ' -e '^' \
-            | GREP_COLOR='01;32' grep --color=always -e '^PASS ' -e '^'
-        if [[ $? -ne 0 ]]; then
-            echo -e "${ESCAPE_ERROR}Testing failed!${ESCAPE_RESET}" >&2
-            exit 1
-        fi
-        ;;
-esac
+            ;;
+    esac
+fi
